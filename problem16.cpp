c755e52999898e554ec3aad3c94e541a803119e0
ahmed lahmidi <ahmedlahmidigit@gmail.com>
2025-11-16 19:24:50 +0100

    Print All Words FRom_AAA TO ZZZ

---
+++ problem16.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cmath>
+#include<string> 
+
+using namespace std;
+
+
+void PrintAllWordsFRom_AAA_TO_ZZZ()
+{
+	cout << "\n";
+	string word = "";
+	for (int i = 65; i <= 90; i++)
+	{
+		for (int j = 65; j <= 90; j++)
+		{
+			for (int k = 65; k <= 90; k++)
+			{
+				word += char(i);
+				word += char(j);
+				word += char(k);
+
+				cout << word << endl;
+
+				word = "";
+			}
+		}
+	}
+}
+
+int main()
+{
+
+	PrintAllWordsFRom_AAA_TO_ZZZ();
+
+	return 0;
+}
+
