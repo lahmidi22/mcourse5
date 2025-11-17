12eea3799754629e8de95e69012e04663ebcbf91
ahmed lahmidi <ahmedlahmidigit@gmail.com>
2025-11-17 22:23:58 +0100

    Generat key

---
+++ problem21.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<string> 
+//#include <cstdlib>    // Include cstdlib for rand() and srand() functions.
+//#include <ctime>      // Include ctime for the time() function, used for seeding the random number generator.
+
+using namespace std;
+
+int ReadNumber(string Message)
+{
+	int number = 0;
+	cout << Message << endl;
+	cin >> number;
+
+	return number;
+}
+int guessNumber(int From, int To)
+{
+
+	// Function to generete a random number
+
+	int number = rand() % (To - From + 1) + From;
+
+
+	return number;
+}
+
+string charGenerate()
+{
+	string word = "";
+
+	for (int i = 1; i <= 4; i++)
+	{
+		word += char(guessNumber(65, 90));
+	}
+	return word;
+}
+string keyGenerate()
+{
+	string fullkey = "";
+
+	for (int i = 1; i <= 4; i++)
+	{
+		if (i < 4)
+		{
+			fullkey += (charGenerate() + char(45));
+		}
+		else
+		{
+			fullkey += (charGenerate());
+		}
+
+	}
+	return fullkey;
+}
+void keys(int number)
+{
+	for (int i = 1; i <= number; i++)
+	{
+		system("color 6f");
+		cout << "Key [ " << i << " ] : " << keyGenerate() << endl;
+	}
+}
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	keys(ReadNumber("enter  how many keys do you want"));
+
+	return 0;
+}
