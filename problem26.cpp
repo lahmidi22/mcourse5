3e14e50ab1a4678a6350bf79870a5d42bc5939ba
ahmed lahmidi <ahmedlahmidigit@gmail.com>
2025-11-25 10:21:19 +0100

    Sum numbers of array

---
+++ problem26.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<string> 
+//#include <cstdlib>    // Include cstdlib for rand() and srand() functions.
+//#include <ctime>      // Include ctime for the time() function, used for seeding the random number generator.
+
+using namespace std;
+
+int RandomNumber(int From, int To)
+{
+	int RandNumber = rand() % (To - From + 1) + From;
+
+	return RandNumber;
+}
+
+void ArrElement(int arr[100], int& length)
+{
+	cout << "enter numbr of element" << endl;
+	cin >> length;
+
+	for (int i = 0; i < length; i++)
+	{
+
+		arr[i] = RandomNumber(1, 100);
+
+	}
+
+}
+
+void PrintArrElement(int arr[100], int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << "\n";
+}
+
+int sumNumber(int arr[100], int length)
+{
+	int sum = 0;
+
+	for (int i = 0; i < length; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+
+}
+
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int arr[100];
+	int length;
+
+	ArrElement(arr, length);
+
+	cout << "array element ";
+	PrintArrElement(arr, length);
+
+	cout << "Min number: ";
+	cout << sumNumber(arr, length);
+
+	return 0;
+}
