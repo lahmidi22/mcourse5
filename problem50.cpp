f1c1adc72b890eeba6fc3289baf01fa11a29ba1a
ahmed lahmidi <ahmedlahmidigit@gmail.com>
2026-01-07 23:55:11 +0100

    My sqrt

---
+++ problem50.cpp
@@ -0,0 +1,30 @@
+#include <iostream>   // Include iostream for input/output operations.
+#include <cmath>      // Include cmath for mathematical functions like sqrt and pow.
+using namespace std;
+
+float MySqrt(float Number)
+{
+    return pow(Number, 0.5);  
+}
+float ReadNumber()
+{
+    float Number;  
+
+   
+    cout << "Please enter a number? ";
+    cin >> Number; 
+
+    return Number;  
+}
+int main()
+{
+    float Number = ReadNumber();  
+
+    
+    cout << "My MySqrt Result : " << MySqrt(Number) << endl;
+
+   
+    cout << "C++ sqrt Result: " << sqrt(Number) << endl;
+
+    return 0;  
+}
